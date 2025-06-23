d491e210ace2a67d9014eafd23ea8c17b5dee97a
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-06-23 22:21:37 +0530

    learned useRef hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import HookExample1 from "./HookExample1";
+import HookExample2 from "./HookExample2";
 
 const App=()=>{
 return(
-    <HookExample1/>
+    <>
+        {/* <HookExample1/> */}
+        <HookExample2/>
+    </>
 )
 }
 
---
+++ src/HookExample2.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react"
+
+const HookExample2=()=>{
+
+    //useref hook is use to create mutable objects which can persists across re-renders
+
+    //a state triggers a re-renders for a component while useRef does not triggers re-renders for a component
+    //but the value persists across renders
+    const [input,setInput]=useState("")
+    const inputRef=useRef(null)
+
+    const [count1,setCount1]=useState(0)
+
+    const count2=useRef(0)
+
+    useEffect(()=>{
+        console.log("abcf")
+        inputRef.current && inputRef.current.focus()
+        inputRef.current.style.backgroundColor="red"
+    },[])
+return(
+    <div>
+        <input type="text" placeholder="type something..." 
+        onChange={(e)=>{setInput(e.target.value)}} ref={inputRef} value={input}/>
+        <p>You Typed: {input}</p>
+
+        <p>count using state: {count1}</p>
+        <button onClick={()=>{setCount1(count1+1)}}>increment using state</button>
+
+        <p>count using ref: {count2.current}</p>
+        <button onClick={()=>{count2.current+=1}}>increment using ref</button>
+    </div>
+)
+}
+export default HookExample2
