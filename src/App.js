62c4d1a96075d9ec42dacdcea189346ca40914b4
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-06-25 00:08:16 +0530

    created dark/light mode switcher using context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import ReactDOM from 'react-dom/client';
 import HookExample1 from "./HookExample1";
 import HookExample2 from "./HookExample2";
 import { ThemeProvider } from "./ThemeProvider";
 import ChildA from "./components/ChildA";
+import "./style.css";
 
 export const UserContext=createContext()
+
+export const ThemeContext=createContext()
 const App=()=>{
 
     const [contextExample,setContextExample]=useState({name:"shipra"})
+
+    const [theme,setTheme]=useState("light")
+
 return(
     <>
         {/* <HookExample1/> */}
@@ -18,7 +24,12 @@ return(
         </ThemeProvider>
 
         <UserContext.Provider value={contextExample}>
-            <ChildA/>
+            <ThemeContext.Provider value={{theme,setTheme}}>
+                <div id="container"
+                style={{background:theme==='light'?'beige':"black"}}>
+                    <ChildA/>
+                </div>
+            </ThemeContext.Provider>
         </UserContext.Provider>
     </>
 )
--- src/components/ChildB.jsx
+++ src/components/ChildB.jsx
@@ -1,11 +1,24 @@
 import { useContext } from "react"
 import { UserContext } from "../App"
+import { ThemeContext } from "../App"
 
 const ChildB=()=>{
     const User=useContext(UserContext)
-    console.log("nanna",User)
+
+    const {theme,setTheme}=useContext(ThemeContext)
+
+    const handleClick=()=>{
+        const changeTheme=theme==='light'?'dark':"light"
+        setTheme(changeTheme)
+    }
 return(
+    <>
     <div>name is:{User.name}</div>
+
+    <div>Theme is: {theme}</div>
+    <button onClick={handleClick}>change theme</button>
+    </>
+    
 )
 }
 export default ChildB
